55f9421589df3a19df8dd01f344f8572c2bc2422
auyu <datealive102164@gmail.com>
2021-08-24 23:42:16 +0800

    docs: separate javascript from html file

---
+++ index.js
@@ -0,0 +1,23 @@
+var table = document.querySelector('table');
+var requestURL = "F720900.json";
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function(){
+    var Data = request.response;
+    showDatas(Data);
+}
+function showDatas(Datajson){
+    for (i=0; i<Datajson.length; i++){
+        var Outside = document.createElement('tr');
+        var Td1 = document.createElement('td');
+        var Td2 = document.createElement('td');
+        Td1.textContent = Datajson[i].Name;
+        Td2.textContent = Datajson[i].Department;
+        Outside.appendChild(Td1);
+        Outside.appendChild(Td2);
+
+        table.appendChild(Outside)
+    }
+}
